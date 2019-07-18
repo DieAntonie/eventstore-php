fb7c491df9608d0e0cfb443b9db5665807838a21
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-07-18 21:22:56 +0200

    Add test code

--- index.php
+++ index.php
@@ -5,4 +5,9 @@ require_once "event/OrderEvents.php";
 
 echo "Hello Wolrd! <br/>";
 
+$eventArr = array(new OrderCreated(3), new StatusChanged("paid"), new StatusChanged("confirmed"));
+$myOrder = new Order($eventArr);
+$myOrder->set_status("shipped");
+echo json_encode($myOrder);
+
 ?>
