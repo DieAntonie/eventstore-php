e0b440b4ee7998d08822ac60fccda31709f70ad9
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-07-21 09:50:27 +0200

    Add MySQL event store

---
+++ utils/MySQLEventStore.php
@@ -0,0 +1,83 @@
+<?php
+
+echo "Loading MySQLEventStore.php... <br/>";
+
+require_once "event/EventStream.php";
+require_once "utils/EventStore.php";
+require_once "utils/Query.php";
+require_once "utils/Tools.php";
+
+/** MySQL `EventStore` implementation */
+class MySQLEventStore extends EventStore
+{    
+    /**
+     * @inheritdoc
+     */
+    function load_stream(string $aggregate_uuid)
+    { 
+        echo __METHOD__."($aggregate_uuid) <br/>";
+        $aggregate = $query ->table('aggregates')
+                            ->where('uuid', '=', $aggregate_uuid)
+                            ->execute();
+
+        $events = $query ->table('events')
+                            ->where('aggregate_uuid', '=', $aggregate_uuid)
+                            ->execute();
+
+        $events_objects = array();
+        foreach ($events as $event)
+            $events_objects[] = $this->_translate_to_object($event);
+        $version = $aggregate[0]->version;
+ 
+        return EventStream($events_objects, $version);
+    }
+
+    private function _translate_to_object($event_model)
+    {
+        echo __METHOD__."(".json_encode($event_model).") <br/>";
+        $class_name = $event_model->name;
+        $kwargs = $event_model->payload;
+        $uuid = $event_model->uuid;
+        return new $class_name($uuid, $kwargs);
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    function append_to_stream(string $aggregate_uuid, int $expected_version = null, array $events)
+    {
+        echo __METHOD__."($aggregate_uuid, $expected_version, ".json_encode($events).") <br/>";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "flashcards";
+        $mysqli = new mysqli($servername, $username, $password, $dbname);
+        $query = new Query($mysqli);
+        if ($expected_version) {
+            $query->table('aggregates')
+                ->update(array("version"), $expected_version + 1)
+                ->where("uuid", "=",  $aggregate_uuid)
+                ->and_where("version", "=", $expected_version)
+                ->execute();
+        } else {
+            $query->table('aggregates')
+                ->insert(array("uuid", "version"), array($aggregate_uuid, 1))
+                ->execute();
+        }
+
+        foreach ($events as $event) {
+            $query->table('events')
+                ->insert(array("uuid",
+                               "aggregate_uuid",
+                               "name",
+                               "payload"), 
+                         array(gen_uuid(),
+                               $aggregate_uuid,
+                               get_class($event),
+                               json_encode($event)))
+                ->execute();
+        }
+    }
+}
+
+?>
